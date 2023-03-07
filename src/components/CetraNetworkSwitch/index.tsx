5f98da84ec4059167fd9e7f759d514b7fc6bb3ae
b3zrazli4n0 <b3zrazli4n0@gmail.com>
2023-03-07 22:42:48 +0300

    fix: 'CetraNetworkSwitch' id selection hotfix

--- src/components/CetraNetworkSwitch/index.tsx
+++ src/components/CetraNetworkSwitch/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useState } from "react";
+import { FC, useCallback } from "react";
 import { Select } from "@chakra-ui/react";
 import { useNetwork } from "wagmi";
 import { DEFAULT_CHAINS } from "../../utils";
@@ -21,15 +21,12 @@ const CetraNetworkSwitch: FC<CetraNetworkSwitchProps> = ({
     onChange,
 }) => {
     const { chain } = useNetwork();
-    const id = chain ? (DEFAULT_CHAINS[0].id === chain.id ? 0 : 1) : 0;
-
-    const [selectedIndex, setSelectedIndex] = useState(id);
+    const id: number = chain ? (DEFAULT_CHAINS[0].id === chain.id ? 0 : 1) : 0;
 
     const handleOnChange = useCallback(
         (e: any) => {
             const index = e.target.selectedIndex;
             onChange?.(index);
-            setSelectedIndex(index);
         },
         [onChange]
     );
@@ -37,19 +34,16 @@ const CetraNetworkSwitch: FC<CetraNetworkSwitchProps> = ({
     return (
         <Select
             bg={bgColor ?? "transparent"}
-            color={selectedIndex === 1 ? "#8247E5" : "#FF4545"}
+            color={id === 1 ? "#8247E5" : "#FF4545"}
             fontFamily="Chakra Petch"
             fontWeight={fontWeight ?? "bold"}
             fontSize={fontSize ?? ["12px", "12px", "16px"]}
             w={w ?? "200px"}
             onChange={handleOnChange}
-            border={
-                selectedIndex === 1 ? "1px solid #8247E5" : "1px solid #FF4545"
-            }
+            border={id === 1 ? "1px solid #8247E5" : "1px solid #FF4545"}
             _hover={{}}
             isDisabled={isDisabled}
-            defaultValue={selectedIndex}
-            value={selectedIndex}
+            value={id}
         >
             <option value="optimism">Optimism</option>
             <option value="polygon">Polygon</option>
